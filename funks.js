f0ae62a763e9c72b4f7713c09ec6378e20132b9e
randomizer333 <markosmid333@gmail.com>
2017-08-25 01:07:42 +0200

    one method now works

--- funks.js
+++ funks.js
@@ -1,22 +1,41 @@
 var ccxt = require ('ccxt')
 
-let poloniex  = new ccxt.poloniex ({ verbose: true });
 /*
 bitfinex.loadMarkets().then((results) => {
 	console.log(results);
 }).catch((error) => {
 	console.error(error);
 });
+
+fetchMarkets ()
+fetchOrderBook(symbol)
+fetchTrades (symbol, [params = {}])
+fetchTicker (symbol)
+
 */
 
-function callAPI(currency) {
-	poloniex.fetchTicker(currency).then((results) => {
-		console.log("done calling")
+function fetchTicker(symbol) {
+	let exchange  = new ccxt.poloniex //({ verbose: true });
+	exchange.fetchTicker(symbol).then((results) => {	
+		console.log("done __________ calling")
+		console.log(results.id)
 	}).catch((error) => {
 		console.error(error)
 	})
 }
 
-console.log("calling API");
-callAPI("BTC/USD");
-console.log("after call");
+function callAPI(symbol) {
+	let exchange  = new ccxt.poloniex //({ verbose: true });
+	exchange.fetchOrderBook(symbol).then((results) => {	
+		console.log("done __________ calling")
+		console.log("Last bid: "+results.bids[0])
+		console.log("Last ask: "+results.asks[0])
+		return results.asks;
+	}).catch((error) => {
+		console.error(error)
+	})
+}
+
+console.log("calling ________________________________ API");
+callAPI("USDT_BTC");
+console.log("_______________Done");
