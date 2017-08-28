85aa4a670640ad4312c83c7d3b7ff7b7f1661834
randomizer333 <markosmid333@gmail.com>
2017-08-28 18:39:59 +0200

    adde the rest of public api calls

--- funks.js
+++ funks.js
@@ -1,41 +1,71 @@
 var ccxt = require ('ccxt')
+let exchange  = new ccxt.poloniex //({ verbose: true });
 
-/*
+
+/*poloniex
 bitfinex.loadMarkets().then((results) => {
 	console.log(results);
 }).catch((error) => {
 	console.error(error);
 });
-
+/*
 fetchMarkets ()
+loadMarkets ([reload])
 fetchOrderBook(symbol)
 fetchTrades (symbol, [params = {}])
 fetchTicker (symbol)
-
+fetchBalance ()
+createOrder (symbol, type, side, amount[, price[, params]])
+createMarketBuyOrder
+createMarketSellOrder (symbol, amount)
+createLimitBuyOrder (symbol, amount, limit)
+createLimitSellOrder ('BTC/USD', 1, 10, { 'type': 'trailing-stop' })
 */
-
-function fetchTicker(symbol) {
-	let exchange  = new ccxt.poloniex //({ verbose: true });
-	exchange.fetchTicker(symbol).then((results) => {	
+function fetchMarkets() {
+	exchange.fetchMarkets().then((results) => {	
 		console.log("done __________ calling")
-		console.log(results.id)
+		console.log(results);
 	}).catch((error) => {
 		console.error(error)
 	})
 }
 
-function callAPI(symbol) {
-	let exchange  = new ccxt.poloniex //({ verbose: true });
+function fetchTrades (symbol) {
+	exchange.fetchTrades().then((results) => {	//"BCH/BTC"  "BTC/USDT"
+		console.log("Trades:");
+		console.log(results);
+	}).catch((error) => {
+		console.error(error)
+	})}
+
+function loadMarkets (symbol) {
+	exchange.loadMarkets().then((results) => {	//"BCH/BTC"  "USDT/BTC"
+		console.log("Last price of "+symbol+": ");
+		console.log(results[symbol].info.last);
+	}).catch((error) => {
+		console.error(error)
+	})}
+
+function fetchOrderBook(symbol) {
 	exchange.fetchOrderBook(symbol).then((results) => {	
-		console.log("done __________ calling")
 		console.log("Last bid: "+results.bids[0])
 		console.log("Last ask: "+results.asks[0])
-		return results.asks;
+		return results;
 	}).catch((error) => {
 		console.error(error)
+	})}
+
+function fetchTicker(symbol) {
+	exchange.fetchTicker(symbol).then((results) => {	
+		console.log(results)
+	}).catch((error) => {
+		console.error(error)	
 	})
 }
 
-console.log("calling ________________________________ API");
-callAPI("USDT_BTC");
-console.log("_______________Done");
+console.log("___________start___________");
+//fetchTicker("USDT/BTC");
+//loadMarkets ("BTC/USDT");
+//setInterval(function(){fetchOrderBook("BTC/USDT")}, 5000);
+//setInterval(function(){loadMarkets ("BTC/USDT")}, 3000);
+setTimeout(function(){console.log("------------end-----------")}, 5000);
