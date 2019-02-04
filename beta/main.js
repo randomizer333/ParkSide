85a72325547e236d63c728773483af5712084735
randomizer333 <markosmid333@gmail.com>
2019-02-04 13:24:45 +0100

    1324422019

--- beta/main.js
+++ beta/main.js
@@ -942,7 +942,7 @@ function runBot(baseCurrency, quoteCurrency, strategy, ticker, exchangeName, sto
             trend2 = trendRSI;     //long term trend
             trend3 = trendMACD;     //technical indicator
             trend4 = change24h;     //24h change % 4
-            if (purchase && (trend > 0) && (trend2 >= 0) && (trend3 >= 0) && (trend4 >= 0)) {                //buy // buy with RSI and MACD (trend2 > 0) | (trend2 >= 0)
+            if (purchase && (trend > 0) && (trend2 >= 0) && (trend3 >= 0) ) {                //buy // buy with RSI and MACD (trend2 > 0) | (trend2 >= 0)&& (trend4 >= 0)
                 orderType = "bougth";
                 round += 1;     //dev
                 console.log("No of purchases done: " + round + " of: " + roundMax);
