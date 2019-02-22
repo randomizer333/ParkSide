2a7ad7cc13dec82a644ba91b2fa90a921c8a8ab1
randomizer333 <markosmid333@gmail.com>
2019-02-22 18:30:59 +0100

    made all common api call with async in api.js

--- beta/api.js
+++ beta/api.js
@@ -2,15 +2,16 @@
 This file should contain all the api callbacks to module ccxt
 */
 
-//Init
+//  Reqiurements
 
-let f = require("./funk.js");
-let keys = require("../keys.json");  //keys file location
-let ccxt = require('ccxt');
-let exchangeName = "binance";
-let exchange;
+let f = require("./funk.js");           //common functions
+let keys = require("../keys.json");     //keys file location
+let ccxt = require('ccxt');             //api module
 
-switch (exchangeName) {
+//  Init
+
+let exchange = "binance";
+switch (exchange) {     //Select exchange
     case "bitstamp":
         exchange = new ccxt.bitstamp({
             apiKey: keys.bitstamp.apiKey,
@@ -56,25 +57,94 @@ switch (exchangeName) {
         break;
 }
 
-/*
-f.cs("Software version: " + ccxt.version);
-f.cs("Available exchanges: " + ccxt.exchanges);
-f.cs(exchange.name);
-f.cs(exchange.urls.www);
-f.cs("Referral link: "+exchange.urls.referral);
-f.cs("Fees: "+exchange.fees.trading.maker);
-*/
-
-//Call functions of CCXT--------
-
-async function change(symbol) {
-    result = await exchange.fetchTicker(symbol);
-    return result.percentage;
+function exInfo() {     //returns JSON of exchange info
+    return {
+        version: ccxt.version,
+        exchange: exchange.name,
+        url: exchange.urls.www,
+        referral: exchange.urls.referral,
+        feeMaker: exchange.fees.trading.maker,
+        feeTaker: exchange.fees.trading.taker,
+        exchanges: ccxt.exchanges
+    }
 }
 
-//Exports of this module--------
+//  Call functions of CCXT
 
+async function change(symbol) {             //returns Variable change percentage of a market
+    r = await exchange.fetchTicker(symbol);
+    return r.percentage;
+}
+async function minAmount(symbol) {          //returns Variable change percentage of a market
+    r = await exchange.loadMarkets();
+    re = r[symbol].limits.amount.min;
+    return re;
+}
+async function wallet() {                   //returns Array of Objects balances of an account
+    r = await exchange.fetchBalance();
+    //f.cs(r);
+    curs = Object.keys(r);
+    vals = Object.values(r.total);
+    let balances = [];
+    parseBlances();
+    function parseBlances() {
+        let j = 0
+        for (i = 0; i < curs.length; i++) {
+            if (vals[i] > 0) {
+                balances[j] = { currency: curs[i + 1], balance: vals[i] }
+                j++
+            }
+        }
+    }
+    return await balances;
+}
+async function balance(currency) {          //returns Array of Objects balances of an account
+    r = await exchange.fetchBalance();
+    return await r[currency].total;
+}
+async function bid(symbol) {                //reurns Array of Objects bid,ask
+    r = await exchange.fetchOrderBook(symbol);
+    return bid = r.bids[0][0];
+}
+async function ask(symbol) {                //reurns Array of Objects bid,ask
+    r = await exchange.fetchOrderBook(symbol);
+    return ask = r.asks[0][0];
+}
+async function cancel(id) {                 //cancels order with id
+    r = await exchange.cancelOrder(id);
+    return "Canceled order" + JSON.stringify(r);
+}
+async function sell(symbol, amount, price) {// symbol, amount, ask 
+    r = await exchange.createLimitSellOrder(symbol, amount, price)
+    orderId = r.id;
+    orderStatus = r.status;
+    sendMail("Sold", msg + "\n" + JSON.stringify(r));
+    orderType = "sold";
+    setTimeout(function () { cancelOrder(orderId) }, timeTicker * 0.9);
+}
+async function buy(symbol, amount, price) { // symbol, amount, bid 
+    r = exchange.createLimitBuyOrder(symbol, amount, price)
+    orderId = r.id;
+    orderStatus = r.status;
+    sendMail("Bougth", msg + "\n" + JSON.stringify(r));  //dev
+    bougthPrice = price;
+    setTimeout(function () { cancelOrder(orderId) }, timeTicker * 0.9);
+}
+
+
+//  Exports of this module
+
+exports.exInfo = exInfo;
+
+exports.balance = balance;
+exports.bid = bid;
+exports.ask = ask;
 exports.change = change;
+exports.wallet = wallet;
+exports.minAmount = minAmount;
+exports.cancel = cancel;
+exports.sell = sell;
+exports.buy = buy;
 
 
 
--- beta/test.js
+++ beta/test.js
@@ -1,21 +1,25 @@
 let a = require("./api.js");
 let f = require("./funk.js");
 
-//mainCascade();
-async function mainCascade(){
+//  main setup
+//setup();
+async function setup(){
     await a.change();
     let balance = await a.balance();
     await a.strategy(balance);
 }
 
-setInterval(function(){getPercentage()},f.minToMs(0.2));
-async function getPercentage() {
-    f.cs(await a.change("BCPT/BTC"));
-    f.cs(await a.change("ENG/BTC"));
-    f.cs(await a.change("ICX/BTC"));
-    return await a.change("BTT/BTC");
-}
-//s();
-async function s(){
-    f.cs(await getPercentage());
+//  main loop
+
+//setInterval(function(){loop()},f.minToMs(ticker));
+let symbol = "BCH/BTC";
+loop(symbol);
+async function loop(symbol) {
+    f.cs(a.exInfo());
+    f.cs(await a.balance("ETH"));
+    f.cs(await a.bid(symbol));
+    f.cs(await a.ask(symbol));
+    f.cs(await a.change(symbol));
+    f.cs(await a.minAmount(symbol));
+    return;
 }
