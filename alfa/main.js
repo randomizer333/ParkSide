ae9f94b99bfaf7605839a83e98edb49071d2c08e
randomizzer333 <markosmid333@gmail.com>
2020-05-09 23:01:29 +0200

    altus

--- alfa/main.js
+++ alfa/main.js
@@ -37,7 +37,7 @@ async function init() {
 let quotes = [    //fiat strategy trading portofio
     //"BTC/USDT",   //in setings
     //cripto base/fiat quote
-    "BNB/USDT",
+    /*"BNB/USDT",
     "ETH/USDT",
 
     "XRP/USDT",
@@ -48,7 +48,7 @@ let quotes = [    //fiat strategy trading portofio
     "XMR/USDT",
     "TRX/USDT",
 
-    "BCH/USDT",
+    "BCH/USDT",*/
 ]
 
 let alts = [    //alt markets 21+3=24
