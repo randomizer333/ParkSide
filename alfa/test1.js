3942104da2b34cc4445a219095fd187ed7be3006
randomizzer333 <markosmid333@gmail.com>
2020-12-14 11:01:02 +0100

    v1412

--- alfa/test1.js
+++ alfa/test1.js
@@ -3,7 +3,35 @@ var send1 = 1;
 var send2 = 2;
 
 let ret = console.log(test(send1))
-function test(send1, send2){
-    
+async function test(send1, send2){
+    console.log(countSeconds())
+    send1 = await delay(5)
+    console.log(send1)
     return send1 + send2
+}
+
+
+async function delay(durationInSeconds){
+    timeMS = await durationInSeconds * 1000
+    return new Promise(resolve => {
+        setTimeout(
+            async () => {
+                resolve(
+                    true
+                );
+            }, timeMS);   //set frequenci
+    });
+}
+
+async function countSeconds(){
+    let c = 1
+    return new Promise(resolve => {
+        setInterval(
+            async () => {
+                resolve(
+                    console.log(c++),
+                    console.dir("jo")
+                );
+            }, 1000);   //set frequenci
+    });
 }
